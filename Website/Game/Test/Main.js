46afa3dac8d06ba345cd537f72323c1fe079ee99
picoriley <acloudy@mail.smu.edu>
2014-02-15 17:52:35 -0600

    Started off by using PreloadJS to load the images and sounds

--- Website/Game/Test/Main.js
+++ Website/Game/Test/Main.js
@@ -51,3 +51,80 @@ var totalLoaded = 0;
 
 var TitleView = new Container();
 
+function Main()
+{
+  canvas = document.getElementById('PongStage');
+  stage = new Stage(canvas);
+  stage.mouseEventsEnabled = true;
+  
+  //Declare all of the images up front, and give each one a unique id
+  manifest = [
+            {src:"bg.png", id:"bg"},
+            {src:"main.png", id:"main"},
+            {src:"startB.png", id:"startB"},
+            {src:"creditsB.png", id:"creditsB"},
+            {src:"credits.png", id:"credits"},
+            {src:"paddle.png", id:"cpu"},
+            {src:"paddle.png", id:"player"},
+            {src:"ball.png", id:"ball"},
+            {src:"win.png", id:"win"},
+            {src:"lose.png", id:"lose"},
+            {src:"playerScore.mp3|playerScore.ogg", id:"playerScore"},
+            {src:"enemyScore.mp3|enemyScore.ogg", id:"enemyScore"},
+            {src:"hit.mp3|hit.ogg", id:"hit"},
+            {src:"wall.mp3|wall.ogg", id:"wall"}
+        ];
+	
+  //The preloader here handles the resource loading, and then lets us load the items from the manifest.
+  preloader = new PreloadJS();
+  preloader.installPlugin(SoundJS);
+  preloader.onProgress = handleProgress;
+  preloader.onComplete = handleComplete;
+  preloader.onFileLoad = handleFileLoad;
+  preloader.loadManifest(manifest);
+  
+  //Set the FPS of the game and link the stage to it.
+  Ticker.setFPS(30);
+  Ticker.addListener(stage); 
+}
+
+//This function lets us follow the progress of the loading operation.
+//We can make a progress bar in here!
+function handleProgress(event)
+{
+  //use event.loaded to get the percentage of the loading
+}
+
+//What gets called when we're done loading.  
+function handleComplete(event) 
+{
+  totalLoaded++;
+  
+  if(manifest.length==totalLoaded)
+  {
+    addTitleView();
+  }
+}
+ 
+//Allows us to discern the files we're loading, and do something different for each.
+function handleFileLoad(event) 
+{
+  //triggered when an individual file completes loading
+      
+  switch(event.type)
+  {
+    case PreloadJS.IMAGE:
+    //image loaded
+      var img = new Image();
+      img.src = event.src;
+      img.onload = handleLoadComplete;
+      window[event.id] = new Bitmap(img);
+    break;
+
+    case PreloadJS.SOUND:
+    //sound loaded
+    handleLoadComplete();
+    break;
+  }
+}
+
