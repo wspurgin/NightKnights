2598cb2d61744c509dabc439d173119a39252b55
Marfle-Bark <eagleguy125@gmail.com>
2014-03-18 21:50:56 -0500

    Working on login.php.

---
+++ public/html/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Welcome to Hibera, Night Knights!</title>
+    </head>
+
+    <body>
+        <h1 id="titlebanner" class="title">NIGHT KNIGHTS</h1>
+
+        Returning Nights
+        <div id="returning" class="infobox">I'm in a box</div>
+
+        New Recruits
+        <div id="newbies" class="infobox">I'm also in a box!</div>
+        
+    </body>
+</html>
