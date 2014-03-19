6305b0875bf9fde89e66a2b02315d4545952bf35
Marfle-Bark <eagleguy125@gmail.com>
2014-03-18 20:13:33 -0500

    Fixed my own directory structure up a bit to test my new vhost.

---
+++ public/website/index.php
@@ -0,0 +1,11 @@
+<--This space intentionally left blank.-->
+
+<html>
+    <head>
+        <title>NightKnights!</title>
+    </head>
+<body>
+    <p>Your virtual host is working, it seems! :3</p>
+</body>
+</html>
+
