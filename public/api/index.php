d28f24aff942d6372a54389aad10c4ef0dcf67c3
Will Spurgin <wspurgin@smu.edu>
2014-03-20 19:48:11 -0500

    Initial api setup

--- public/api/index.php
+++ public/api/index.php
@@ -1,1 +1,14 @@
-<--This space intentionally left blank.-->
+<?
+require '../../vendor/autoload.php';
+require_once('../../lib/routes.php');
+require_once('../../lib/api.php');
+
+session_cache_limiter(false);
+
+
+$app = new \Slim\Slim();
+$app->log->setEnabled(true);
+
+// get all routes from routes.php
+
+$app->run();
