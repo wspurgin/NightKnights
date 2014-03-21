13d5d22bf0135f093bfcc3a88148f3169fe96cff
Will Spurgin <wspurgin@smu.edu>
2014-03-20 20:31:46 -0500

    Boilerplated some basic API files

---
+++ lib/routes.php
@@ -0,0 +1,7 @@
+<?
+/*
+Route
+	type GET POST PUT DELETE
+	url [string]
+	callable [string] where to go with url
+*/
--- public/api/index.php
+++ public/api/index.php
@@ -1,7 +1,7 @@
 <?
 require '../../vendor/autoload.php';
-require_once('../../lib/routes.php');
-require_once('../../lib/api.php');
+require_once('../../lib/config.php');
+
 
 session_cache_limiter(false);
 
@@ -9,6 +9,18 @@ session_cache_limiter(false);
 $app = new \Slim\Slim();
 $app->log->setEnabled(true);
 
-// get all routes from routes.php
+// get all the bootstraped routes from config
+foreach ($ROUTES as $route) {
+	if($route->type == Route::GET)
+		$app->get($route->url, $route->callable);
+	elseif ($route->type == Route::POST)
+		# code...
+	elseif ($route->type == Route::PUT)
+		# code...
+	elseif ($route->type == Route::DELETE)
+		# code...
+	else
+		// not supported route type
+}
 
 $app->run();
