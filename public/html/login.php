be952bdb476faf050b71d80feab20f222c8c8403
Marfle-Bark <eagleguy125@gmail.com>
2014-03-20 19:46:48 -0500

    Started styling master.css, fixed php include statement + copyright.html

--- public/html/login.php
+++ public/html/login.php
@@ -2,17 +2,27 @@
 <html>
     <head>
         <title>Welcome to Hibera, Night Knights!</title>
+        <meta charset="utf-8" \>
+
+
     </head>
 
     <body>
         <h1 id="titlebanner" class="title">NIGHT KNIGHTS</h1>
 
         Returning Nights
-        <div id="returning" class="infobox">I'm in a box</div>
+        <div id="returning" class="infobox">
+            I'm in a box
+        </div>
 
         New Recruits
-        <div id="newbies" class="infobox">I'm also in a box!</div>
+        <div id="newbies" class="infobox">
+            I'm also in a box!
+        </div>
 
-        <div id="copyright"> </div>
+        <div id="copyright">
+            <?php include("modules/copyright.html"); ?>
+        </div>
+
     </body>
 </html>
