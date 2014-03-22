3c242ac52b7616846f04a5274d62bad2704e71d1
Marfle-Bark <eagleguy125@gmail.com>
2014-03-22 03:01:11 -0500

    Fixed buttons and other minor elements of login.php. Added login.css to handle things only used on login.php (currently empty). Made improvements to master.css, and login/account creation modules.

--- public/html/login.php
+++ public/html/login.php
@@ -3,6 +3,7 @@
 <head>
   <title>Night Knights - Login</title>
   
+  <link rel="stylesheet" type="text/css" href="../css/login.css">
   <?php include("modules/commonHead.html"); ?>
 </head>
 
@@ -12,21 +13,21 @@
     <h1>NIGHT KNIGHTS</h1>
   </header>
 
-  <div class="center" id="login">
-    <p class="infocaption">Returning Nights</p>
+  <div class="center" id="logindiv">
+    <p class="infocaption">Returning Knights</p>
     <div id="returning" class="infobox">
       <?php include("modules/login.html"); ?>
     </div>
   </div>
 
-  <div class="center" id="account">
+  <div class="center" id="accountdiv">
     <p class="infocaption">New Recruits</p>
     <div id="newbies" class="infobox">
       <?php include("modules/createAccount.html"); ?>
     </div>
   </div>
 
-  <div class="center" id="story">
+  <div class="center" id="storydiv">
     <p class="infocaption">STORY</p>
     <div id="blurb" class="infobox words">
       <p>STORY GOES HERE!</p>
