c36d536d1b6f835812e0e3ac0ecd6ae63d5d57a9
Marfle-Bark <eagleguy125@gmail.com>
2014-03-27 20:21:02 -0500

    Leaderboards are 'done' for iteration one. I think.

--- public/html/leaderboards.php
+++ public/html/leaderboards.php
@@ -18,7 +18,6 @@
 
     <div class="left" id="topten">
       <p class="infocaption">Top Ten Knights</p>
-      <div class="infobox">
         <table class="infotable">
               <thead>
                 <tr>
@@ -91,12 +90,82 @@
                 </tr>
               </tbody>
             </table>
-          </div>
-
     </div>
 
     <div class="right" id="myrank">
       <p class="infocaption">My Ranking</p>
+      <table class="infotable">
+              <thead>
+                <tr>
+                  <th scope="col">Rank</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Level</th>
+                  <th scope="col">Experience</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                </tr>
+                <tr>
+                  <td>5</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                </tr>
+                <tr>
+                  <td>6</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                </tr>
+                <tr>
+                  <td>7</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                </tr>
+                <tr>
+                  <td>8</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                </tr>
+                <tr>
+                  <td>9</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                </tr>
+                <tr>
+                  <td>10</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                  <td>Marf</td>
+                </tr>
+              </tbody>
+            </table>
     </div>
 
   </div>  <!--This is the end of div #leaderwrapper-->
