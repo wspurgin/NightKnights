b9e0ec454d60306c994afb0375e2260c8fbcc14f
Marfle-Bark <eagleguy125@gmail.com>
2014-03-28 22:58:36 -0500

    PRESS THE MARFLE BARK BUTTON YOU GUISE

--- public/js/login.js
+++ public/js/login.js
@@ -11,32 +11,63 @@
 
     $(document).ready(function() {
 
-      $('#loginform').submit(function() {
+      $('#loginform').submit(function(event) {
 
         var info = {
-            'email': $('#loginEmail').val(),
-            'password': $('#loginPassword').val()
-          };
+          'email':$('#loginEmail').val(),
+          'password':$('#loginPassword').val()
+        };
 
+        console.log('running AJAX call... NOW!');
         $.ajax(
         {
-          url: '/api/login',
+          url: '../api/login',
+          type: "POST",
+          data: info,
+          dataType: "json",
+          contentType: "application/json",
+
+          success: function (data) {
+            console.log('success:');
+            //alert('success!');
+            window.location.replace("home.php");
+          },
+          error: function(data) {
+            console.log('error:');
+            alert('error :c');
+            window.location.replace("home.php");
+          }
+        });
+    }); //end of #loginform.submit()
+
+      $('#test').click(function(event) {
+        var info = {
+          'email':'ebusbee@smu.edu',
+          'password':'marflebark'
+        };
+
+        console.log('running AJAX call... NOW!');
+        $.ajax(
+        {
+          url: '../api/login',
           type: "POST",
           data: JSON.stringify(info),
           dataType: "json",
           contentType: "application/json; charset=utf-8",
 
-          success: function (data) {
-            alert('success!');
-            console.log(data);
+          success: function () {
+            console.log('success:');
             window.location.replace("home.php");
           },
-          error: function(data) {
-            alert('error :c');
-            console.log(data);
-            window.location.replace("home.php");
+          error: function(text) {
+            console.log('error, sent:');
+            console.log(JSON.stringify(info));
+            console.log('responseText:');
+            console.log(text.responseText);
+            console.log('statusText:');
+            console.log(text.statusText);
           }
         });
-    }); //end of #loginform.submit()
+    }); //end of #test.submit()
 
   }); //end of document.ready()
