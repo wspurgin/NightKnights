134e95e43a33004e0439f79b639061e6d7d49336
Marfle-Bark <eagleguy125@gmail.com>
2014-03-29 12:02:57 -0500

    login.js is still borked, but the account page is now there.

--- public/html/account.php
+++ public/html/account.php
@@ -9,7 +9,36 @@
 
   <?php include("modules/navbar.html"); ?>
 
-  <p>Content Goes Here</p>
+  <div class="spacer"></div>
+
+  <div class="twocol" id="accountwrapper">
+    <div class="left" id="accountleft">
+
+      <p class="infocaption">My Night Knights Account</p>
+      <div class="infobox" id="accountstats">
+        <p>Username: Marf</p>
+        <p>Email: bark@marf.com</p>
+      </div>
+
+    </div>
+
+    <div class="right" id="accountright">
+
+      <p class="infocaption">Change Password</p>
+      <div class="infobox" id="accountpasswordreset">
+        <form method="POST" id="passwordresetform">
+          <ul>
+            <li><label for="oldpassword">Old Password: </label><input type="text" id="oldpassword"></li>
+            <li><label for="newpassword">New Password: </label><input type="text" id="newpassword"></li>
+            <li><label for="repeatpassword">Repeat New Password: </label><input type="text" id="repeatpassword"></li>
+            <li><input type="submit" id="changepassword" method="POST" value="Change Password"></li>
+          </ul>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <div class="spacer"></div>
 
   <?php include("modules/copyright.html"); ?>
 
--- public/js/login.js
+++ public/js/login.js
@@ -1,73 +1,54 @@
 /*
   login.js: only meant to interact with login.php, as that *should* be the only page allowing for
   account creation and login.
+  */
 
-  TODO:
-    1. Implement login call.
-    2. Implement account creation call.
-    3. Handle errors on login.
-    4. Handle errors on account creation.
-    */
+  $(document).ready(function() {
 
-    $(document).ready(function() {
+    $('#loginform').submit(function() {
 
-      $('#loginform').submit(function(event) {
+      //setup information from form (validation happens at form-level, not here)
+      var info = {
+        'email':$('#loginEmail').val(),
+        'password':$('#loginPassword').val()
+      };
 
-        var info = {
-          'email':$('#loginEmail').val(),
-          'password':$('#loginPassword').val()
-        };
+      info = JSON.stringify(info);    //stringify the information
+      info = info.replace(/"/g, "'"); //replace " with ' so things don't hiccup
 
-        console.log('running AJAX call... NOW!');
-        $.ajax(
-        {
-          url: '../api/login',
-          type: "POST",
-          data: info,
-          dataType: "json",
-          contentType: "application/json",
+      var testinfo = {
+        'email':'ebusbee@smu.edu',
+        'password':'marflebark'
+      };
 
-          success: function (data) {
-            console.log('success:');
-            //alert('success!');
-            window.location.replace("home.php");
-          },
-          error: function(data) {
-            console.log('error:');
-            alert('error :c');
-            window.location.replace("home.php");
+      testinfo = JSON.stringify(testinfo);
+      //testinfo = testinfo.replace(/"/g, "'");
+
+      $.ajax({
+        url: '../api/login',
+        type: 'POST',
+        data: testinfo,
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+
+        error: function(text) {
+          console.log('error, sent:');
+          console.log(testinfo);
+          console.log('responseText:');
+          console.log(text.responseText);
+          console.log('statusText:');
+          console.log(text.statusText);
+          alert('AJAX call unsuccessful. See console for details before closing this window.');
+        },
+
+        success: function (data) {
+            alert('AJAX call was successful.');
+            //TODO: session logic
+            //window.location.replace('home.php');
           }
-        });
+          
+        }); //end of AJAX call
+
     }); //end of #loginform.submit()
-
-      $('#test').click(function(event) {
-        var info = {
-          'email':'ebusbee@smu.edu',
-          'password':'marflebark'
-        };
-
-        console.log('running AJAX call... NOW!');
-        $.ajax(
-        {
-          url: '../api/login',
-          type: "POST",
-          data: JSON.stringify(info),
-          dataType: "json",
-          contentType: "application/json; charset=utf-8",
-
-          success: function () {
-            console.log('success:');
-            window.location.replace("home.php");
-          },
-          error: function(text) {
-            console.log('error, sent:');
-            console.log(JSON.stringify(info));
-            console.log('responseText:');
-            console.log(text.responseText);
-            console.log('statusText:');
-            console.log(text.statusText);
-          }
-        });
-    }); //end of #test.submit()
 
   }); //end of document.ready()
